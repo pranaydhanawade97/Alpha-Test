a5b867a994d33ba32afd1ba5520a6846ab4edce8
pranay <pranay@gmail.com>
2023-03-15 23:31:17 +0530

    new chnages

--- JavaFullCource/src/print.java
+++ JavaFullCource/src/print.java
@@ -5,6 +5,10 @@ public class print {
 		// TODO Auto-generated method stub
 		
 		System.out.println("hello");
+		System.out.println("hello1");
+		System.out.println("hello2");
+		System.out.println("hello3");
+		
 	}
 
 }
